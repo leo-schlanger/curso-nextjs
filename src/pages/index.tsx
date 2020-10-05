397ed4fa35a363188f6010789584a5272b0ad835
Schlanger <leoschlanger@gmail.com>
2020-10-05 09:29:14 -0300

    studying types of data fetching with next

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { GetServerSideProps } from "next";
+import { Title } from "../../styles/pages/Home";
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface IHomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: IHomeProps) {
+  return (
+    <div>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => (
+            <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const response = await fetch("http://localhost:3333/recommended");
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts,
+    },
+  };
+};
